44659dcffa083899623f4ec5a3032772c8c1739b
Güven Can Yıldız <yildiz.guvencan@gmail.com>
2021-06-14 06:42:24 +0300

    style

--- filozoflar.c
+++ filozoflar.c
@@ -17,23 +17,23 @@ void sit(int ph_index);
 
 int main()
 {
-        initialize();
+    initialize();
         
-        int i;
-        pthread_t philosophers[PH_COUNT];
-        for(i=0;i<PH_COUNT;i++){
-		pthread_create(&philosophers[i],NULL,philosopher,(void *) &i);
+    int i;
+    pthread_t philosophers[PH_COUNT];
+    for(i=0;i<PH_COUNT;i++){
+	pthread_create(&philosophers[i],NULL,philosopher,(void *) &i);
 	}
 
-        for(i=0;i<PH_COUNT;i++)
-                pthread_join(philosophers[i],NULL);
+    for(i=0;i<PH_COUNT;i++)
+        pthread_join(philosophers[i],NULL);
 }
 
 void initialize()
 {
-        sem_init(&chair,0,PH_COUNT-1);
+    sem_init(&chair,0,PH_COUNT-1);
 	
-        int i;
+    int i;
 	for(i=0;i<PH_COUNT;i++)
 		sem_init(&chopstick[i],0,1);
 }
@@ -43,7 +43,7 @@ void * philosopher(void * arg)
 	int ph_index=*(int *)arg;
 
 	sem_wait(&chair);
-        sit(ph_index);
+    sit(ph_index);
 	sem_wait(&chopstick[ph_index]);
 	sem_wait(&chopstick[(ph_index+1)%PH_COUNT]);
 
@@ -53,12 +53,12 @@ void * philosopher(void * arg)
 	sem_post(&chopstick[(ph_index+1)%PH_COUNT]);
 	sem_post(&chopstick[ph_index]);
 	sem_post(&chair);
-        think(ph_index);
+    think(ph_index);
 }
 
 void sit(int ph_index)
 {
-       printf("\nPhilosopher %d has sat on chair",ph_index); 
+    printf("\nPhilosopher %d has sat on chair",ph_index); 
 }
 void eat(int ph_index)
 {
